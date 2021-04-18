193087b97cf17de6225436e7a324656ba126cab5
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2021-04-17 22:15:08 -0300

    Initial Commit, basic folder structure

---
+++ app/src/main/java/com/schwarz/pedro/southmarket_android/ui/core/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.schwarz.pedro.southmarket_android.ui.core.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.schwarz.pedro.southmarket_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
